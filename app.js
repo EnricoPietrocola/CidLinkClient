f0f26019ca76697dd87bb9a77a2a533eb66e48a9
ePietrocola <enricopietrocola@gmail.com>
2021-06-09 12:57:33 +0200

    Updated client to new version working with server and max Client

--- app.js
+++ app.js
@@ -1,14 +1,65 @@
-//this basic client sends a message to the main server, which could then broadcast to ALL other users (if set to)
-
+const path = require('path');
 const io = require("socket.io-client");
-const ioClient = io.connect("http://127.0.0.1:5000");
-const roomName = 'roomTest';
 
-ioClient.emit('join', roomName)
-//ioClient.emit('echo', 'hello world');
+let address = "http://127.0.0.1:5000";
+let ioClient = io.connect(address);
 
-setTimeout(() => ioClient.emit('datachannel', roomName, "hello world"), 1000);
+let roomName;
+let password = "";
 
-ioClient.on('echo', (msg)=> {
-    console.log(msg)
-})
+function setRoomName(msg){
+    roomName = msg;
+    ioClient.emit('join', roomName, password)
+}
+
+function setPassword (msg){
+    password = msg
+}
+
+function connect(newAddress){
+    address = newAddress
+    ioClient = io.connect(null, {'force new connection': false}); //disconnect, io.disconnect() is been buggy for long in the history of socketio apparently
+    ioClient = io.connect(address)
+    makeClient()
+}
+
+function send (msg) {
+    ioClient.emit("datachannel", roomName, msg);
+}
+
+function makeClient() {
+    ioClient.on('datachannel', (msg) => {
+        Max.post(msg)
+        Max.outlet(msg)
+
+    })
+
+    ioClient.on('objchannel', (msg) => {
+        Max.setDict(dictIdIn, msg)
+        Max.outlet("bang")
+    })
+
+    ioClient.on('systemchannel', (msg) => {
+        Max.post(msg)
+        Max.outlet(msg)
+    })
+
+    ioClient.on("disconnect", (msg) => {
+        if (msg !== undefined && msg !== null) {
+            if (isJson(msg)) {
+                msg = JSON.parse(msg)
+            } else {
+
+            }
+        }
+    })
+
+    function isJson(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+}
